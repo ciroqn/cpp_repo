d2769e20c2f2ecb3b9dc75831dcd2d11bcd47b51
Marcus Watson <79422987+ciroqn@users.noreply.github.com>
2023-09-07 14:21:07 +0100

    Create header.hpp

---
+++ game_one/header.hpp
@@ -0,0 +1,12 @@
+// function declarations to be inlcuded in main.cpp
+
+#include <vector>
+
+void display_misses(int misses);
+
+// Declare functions
+void greet();
+
+void end_game(std::string answer, std::string codeword);
+
+void display_status(std::vector<char> incorrect, std::string answer);
