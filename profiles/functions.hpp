c8113fdbb713938edbed1cc4bb30088b23d06544
Marcus Watson <79422987+ciroqn@users.noreply.github.com>
2023-09-08 11:31:57 +0100

    construct class members

--- profiles/functions.hpp
+++ profiles/functions.hpp
@@ -1,1 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <string>
 
+class Profile {
+
+  private:
+    std::string name;
+    int age;
+    std::string city;
+    std::string country;
+    std::vector<std::string> hobbies;
+
+  // public methods to manipulate private data
+  public:
+    //constructor
+    Profile(std::string name, int age, std::string city, std::string country);
+
+    // prints profile info
+    std::string view_profile();
+
+    // adds hobby to hobbies vector
+    void add_hobby(std::string new_hobby);
+
+};
