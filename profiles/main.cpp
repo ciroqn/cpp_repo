9298fb743877592261cef89aa8137c6a2519d28f
Marcus Watson <79422987+ciroqn@users.noreply.github.com>
2023-09-08 11:37:07 +0100

    Update main.cpp

--- profiles/main.cpp
+++ profiles/main.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+#include "profile.hpp"
+#include <string>
 
+int main() {
+
+  // instantiate object using Profile class with necessary parameters
+  Profile joe("Joe Bloggs", 30, "Erewhon", "Country Zog");
+
+  // add hobby to hobbies vector
+  joe.add_hobby("reading");
+  joe.add_hobby("hiking");
+  joe.add_hobby("pretending to be productive");
+  joe.add_hobby("daydreaming");
+
+  // print out profile info, defined in functions.cpp
+  std::cout << joe.view_profile() << "\n";
+
+}
